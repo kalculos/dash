0824063cc688090a29397219b03f12a63c899742
iceBear67 <icebear67@sfclub.cc>
2022-12-17 17:53:25 +0800

    Add: (PlatformAdapter) `getFriend/ChatGroup/Contact`

--- dash-core-api/src/main/java/io/ib67/dash/adapter/PlatformAdapter.java
+++ dash-core-api/src/main/java/io/ib67/dash/adapter/PlatformAdapter.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.ApiStatus;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @ApiStatus.AvailableSince("0.1.0")
@@ -15,15 +16,35 @@ public abstract class PlatformAdapter {
     @Getter
     protected final String platformId;
 
-    public List<Friend> getFriends() {
+    public List<? extends Friend> getFriends() {
         return getAllContacts().stream().filter(it -> it instanceof Friend).map(it -> (Friend) it).toList();
     }
 
-    public List<ChatGroup> getChatGroups() {
+    public abstract Optional<? extends Contact> getContact(String platformId);
+
+    public abstract Optional<? extends Contact> getContact(long id); // todo: integrate dash data-store to map UniversalUID -> PlatformUID
+
+    public Optional<? extends Friend> getFriend(long id) {
+        return getContact(id).filter(it -> it instanceof Friend).map(it -> (Friend) it);
+    }
+
+    public Optional<? extends Friend> getFriend(String platformId) {
+        return getContact(platformId).filter(it -> it instanceof Friend).map(it -> (Friend) it);
+    }
+
+    public Optional<? extends ChatGroup> getGroup(long id) {
+        return getContact(id).filter(it -> it instanceof ChatGroup).map(it -> (ChatGroup) it);
+    }
+
+    public Optional<? extends ChatGroup> getGroup(String platformId) {
+        return getContact(platformId).filter(it -> it instanceof ChatGroup).map(it -> (ChatGroup) it);
+    }
+
+    public List<? extends ChatGroup> getChatGroups() {
         return getAllContacts().stream().filter(it -> it instanceof ChatGroup).map(it -> (ChatGroup) it).toList();
     }
 
-    public abstract List<Contact> getAllContacts();
+    public abstract List<? extends Contact> getAllContacts();
 
     public abstract Contact getPlatformBot();
 }
