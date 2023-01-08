a2d561ec71bd27bf6685186af144d4fa2fa2bc8b
iceBear67 <icebear67@sfclub.cc>
2023-01-08 22:05:51 +0800

    Add MessagePreSendEvent & MessagePostSendEvent

--- dash-core-api/src/main/java/io/ib67/dash/event/ipc/message/MessagePostSendEvent.java
+++ dash-core-api/src/main/java/io/ib67/dash/event/ipc/message/MessagePostSendEvent.java
@@ -8,21 +8,13 @@ import org.jetbrains.annotations.ApiStatus;
 
 import java.time.Instant;
 
-/**
- * A MessagePreSendEvent is sent before
- */
 @ApiStatus.AvailableSince("0.1.0")
-public class MessagePreSendEvent<M extends AbstractMessage<?>> extends IpcEvent {
+public class MessagePostSendEvent<M extends AbstractMessage<?>> extends IpcEvent {
     @Getter
     @Setter
     protected M message;
-
-    public MessagePreSendEvent(boolean local, Instant time, M message) {
+    public MessagePostSendEvent(boolean local, Instant time, M message) {
         super(local, time);
         this.message = message;
-    }
-
-    public MessagePostSendEvent<M> toPostSend() {
-        return new MessagePostSendEvent<>(local, time, message);
     }
 }
