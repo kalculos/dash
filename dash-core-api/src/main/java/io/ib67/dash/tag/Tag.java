7fa6ed346f88b5d3aec5acc440fc9764c1213ea4
iceBear67 <icebear67@sfclub.cc>
2023-01-12 18:20:19 +0800

    Fix(dash-core-api): Tag

--- dash-core-api/src/main/java/io/ib67/dash/tag/Tag.java
+++ dash-core-api/src/main/java/io/ib67/dash/tag/Tag.java
@@ -1,6 +1,9 @@
 package io.ib67.dash.tag;
 
-public interface Tag {
+import org.jetbrains.annotations.ApiStatus;
+
+@ApiStatus.AvailableSince("0.1.0")
+public sealed interface Tag permits SimpleTag {
     static Tag of(String identifier){
         return new SimpleTag(identifier);
     }
