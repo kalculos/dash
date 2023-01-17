84231fa764537f7188d4b01e180664013c0d0afe
iceBear67 <icebear67@sfclub.cc>
2023-01-17 21:04:46 +0800

    Annotate Constant with `@UtilityClass`

--- dash-core-impl/src/main/java/dash/internal/Constant.java
+++ dash-core-impl/src/main/java/dash/internal/Constant.java
@@ -1,5 +1,8 @@
 package dash.internal;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class Constant {
     public static final boolean ALLOW_ASYNC_UNSUBSCRIBE = Boolean.getBoolean("dash.asyncHandlers.mutatePipeline");
 }
