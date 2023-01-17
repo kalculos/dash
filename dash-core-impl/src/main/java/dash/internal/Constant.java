60908ea54bd2d3888e4ea9e8100cb752fa1eb19e
iceBear67 <icebear67@sfclub.cc>
2023-01-17 22:19:54 +0800

    (coverage) add tests for SimpleAdapterRegistry and SimpleEventRegistry

--- dash-core-impl/src/main/java/dash/internal/Constant.java
+++ dash-core-impl/src/main/java/dash/internal/Constant.java
@@ -5,4 +5,5 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Constant {
     public static final boolean ALLOW_ASYNC_UNSUBSCRIBE = Boolean.getBoolean("dash.asyncHandlers.mutatePipeline");
+    public static final boolean ALLOW_ILLEGAL_EVENT_TYPE = Boolean.getBoolean("dash.allowIllegalEventType");
 }
---
+++ dash-core-impl/src/test/java/dash/test/MockBot.java
@@ -0,0 +1,14 @@
+package dash.test;
+
+import io.ib67.dash.AbstractBot;
+
+public class MockBot extends AbstractBot {
+    public MockBot() {
+        super("mockBot", null);
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+}
