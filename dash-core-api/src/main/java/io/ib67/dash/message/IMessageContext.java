b70b17da02e5e81280d15929a08ce6648231292c
iceBear67 <icebear67@sfclub.cc>
2023-02-25 14:20:30 +0800

    (dash-core-api) Add `getOrDefault` and `computeIfAbsent` to IMessageContext

--- dash-core-api/src/main/java/io/ib67/dash/message/IMessageContext.java
+++ dash-core-api/src/main/java/io/ib67/dash/message/IMessageContext.java
@@ -2,6 +2,8 @@ package io.ib67.dash.message;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.function.Function;
+
 /**
  * A MessageContext stores additional metadata involved in a {@link AbstractMessage}, And they are accessible with {@link ContextKey}.<br>
  * Getting or putting data from MessageContext is very fast, however you'll have to waste some memory when you have a large amount of context keys.<br>
@@ -43,5 +45,35 @@ public interface IMessageContext {
         boolean hasPreviously = has(key);
         put(key,null);
         return hasPreviously;
+    }
+
+    /**
+     * Returns the default value if not found in the context.
+     * @param key key
+     * @param defaultValue fallback
+     * @return fallback or value
+     * @param <T> type of value
+     */
+    default <T> T getOrDefault(@NotNull ContextKey<T> key, T defaultValue){
+        if(!has(key)){
+            return defaultValue;
+        }
+        return get(key);
+    }
+
+    /**
+     * Generates and puts the value if not found
+     * @param key key
+     * @param mapper function
+     * @return value
+     * @param <T> type of value
+     */
+    default <T> T computeIfAbsent(@NotNull ContextKey<T> key, @NotNull Function<ContextKey<T>,T> mapper){
+        if(!has(key)){
+            var r = mapper.apply(key);
+            put(key,r);
+            return r;
+        }
+        return get(key);
     }
 }
