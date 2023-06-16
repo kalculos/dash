988782b0eed15fbd3b5167ffff7386c3a98dd77d
iceBear67 <icebear67@sfclub.cc>
2023-06-16 17:15:51 +0800

    Refactored User Component

--- dash-core-api/src/main/java/io/ib67/dash/context/IContext.java
+++ dash-core-api/src/main/java/io/ib67/dash/context/IContext.java
@@ -24,6 +24,7 @@
 
 package io.ib67.dash.context;
 
+import io.ib67.dash.message.internal.UnorderedEventContext;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Function;
@@ -33,11 +34,16 @@ import java.util.function.Function;
  */
 public interface IContext {
 
+    static IContext create() {
+        return new UnorderedEventContext();
+    }
+
     /**
      * Put a value indexed by the key into the context.
-     *
+     * <p>
      * If a null is provided as the value, {@link #has(ContextKey)} will always return false for your key.
-     * @param key index
+     *
+     * @param key   index
      * @param value to be stored
      */
     <V> void put(@NotNull ContextKey<V> key, V value);
@@ -45,14 +51,16 @@ public interface IContext {
     /**
      * Get an object from the context. Improper use may lead to {@link ClassCastException}.<br />
      * Example: <code>context.get(KEY);</code>
+     *
      * @param key the key
-     * @return user object or null
      * @param <T> type of value
+     * @return user object or null
      */
     <T> T get(@NotNull ContextKey<T> key);
 
     /**
      * Check if the context contains something related to the key.
+     *
      * @param key key
      * @return if found. Also return false if you set the value to null.
      */
@@ -60,24 +68,26 @@ public interface IContext {
 
     /**
      * Remove something from the context.
+     *
      * @param key the key
      * @return if there was a key-value pair present.
      */
-    default boolean remove(@NotNull ContextKey<?> key){
+    default boolean remove(@NotNull ContextKey<?> key) {
         boolean hasPreviously = has(key);
-        put(key,null);
+        put(key, null);
         return hasPreviously;
     }
 
     /**
      * Returns the default value if not found in the context.
-     * @param key key
+     *
+     * @param key          key
      * @param defaultValue fallback
+     * @param <T>          type of value
      * @return fallback or value
-     * @param <T> type of value
      */
-    default <T> T getOrDefault(@NotNull ContextKey<T> key, T defaultValue){
-        if(!has(key)){
+    default <T> T getOrDefault(@NotNull ContextKey<T> key, T defaultValue) {
+        if (!has(key)) {
             return defaultValue;
         }
         return get(key);
@@ -85,15 +95,16 @@ public interface IContext {
 
     /**
      * Generates and puts the value if not found
-     * @param key key
+     *
+     * @param key    key
      * @param mapper function
+     * @param <T>    type of value
      * @return value
-     * @param <T> type of value
      */
-    default <T> T computeIfAbsent(@NotNull ContextKey<T> key, @NotNull Function<ContextKey<T>,T> mapper){
-        if(!has(key)){
+    default <T> T computeIfAbsent(@NotNull ContextKey<T> key, @NotNull Function<ContextKey<T>, T> mapper) {
+        if (!has(key)) {
             var r = mapper.apply(key);
-            put(key,r);
+            put(key, r);
             return r;
         }
         return get(key);
