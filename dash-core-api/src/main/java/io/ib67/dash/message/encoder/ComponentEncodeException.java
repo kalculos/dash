89ccb4de2b8b4bc30195663995f15f11062e6024
iceBear67 <icebear67@sfclub.cc>
2023-06-24 22:29:54 +0800

    chore(dash-core-api): Some `@Generated` for ComponentEncodeException to escape test coverage check

--- dash-core-api/src/main/java/io/ib67/dash/message/encoder/ComponentEncodeException.java
+++ dash-core-api/src/main/java/io/ib67/dash/message/encoder/ComponentEncodeException.java
@@ -1,6 +1,7 @@
 package io.ib67.dash.message.encoder;
 
 import io.ib67.dash.message.feature.IMessageComponent;
+import lombok.Generated;
 import lombok.Getter;
 import org.jetbrains.annotations.ApiStatus;
 
@@ -14,26 +15,32 @@ public class ComponentEncodeException extends RuntimeException{
      */
     @Getter
     private final IMessageComponent component;
+
+    @Generated
     public ComponentEncodeException(IMessageComponent component) {
         super();
         this.component = component;
     }
 
+    @Generated
     public ComponentEncodeException(String message, IMessageComponent component) {
         super(message);
         this.component = component;
     }
 
+    @Generated
     public ComponentEncodeException(String message, Throwable cause, IMessageComponent component) {
         super(message, cause);
         this.component = component;
     }
 
+    @Generated
     public ComponentEncodeException(Throwable cause, IMessageComponent component) {
         super(cause);
         this.component = component;
     }
 
+    @Generated
     protected ComponentEncodeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, IMessageComponent component) {
         super(message, cause, enableSuppression, writableStackTrace);
         this.component = component;
