6f764aaa300e73f155a0dc708ec18abce58c1f24
iceBear67 <icebear67@sfclub.cc>
2023-06-24 21:33:37 +0800

    chore(dash-core-api): Add `@Nullable` to param `message` for IComponentEncoder#encode

--- dash-core-api/src/main/java/io/ib67/dash/message/encoder/IComponentEncoder.java
+++ dash-core-api/src/main/java/io/ib67/dash/message/encoder/IComponentEncoder.java
@@ -3,8 +3,9 @@ package io.ib67.dash.message.encoder;
 import io.ib67.dash.message.AbstractMessage;
 import io.ib67.dash.message.feature.IMessageComponent;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Nullable;
 
 @ApiStatus.AvailableSince("0.1.0")
 public interface IComponentEncoder<T extends IMessageComponent> {
-    boolean encode(T component, IComponentAppendable appendable, AbstractMessage message);
+    boolean encode(T component, IComponentAppendable appendable, @Nullable AbstractMessage message);
 }
